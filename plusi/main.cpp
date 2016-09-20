53121dfc9810e0905466f7a2edd3a906f1f8e745
Ivan Trofimov <richard1997dog@gmail.com>
2016-09-20 20:26:26 +0300

    Initial Commit

---
+++ plusi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  plusi
+//
+//  Created by Иван Трофимов on 20.09.16.
+//  Copyright © 2016 Иван Трофимов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
